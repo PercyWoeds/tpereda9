1fd7394610dac95bcdd1a797b1e031e7a8ac7124
PercyWoeds <percywoeds@gmail.com>
2019-04-29 15:14:25 +0000

    a

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -286,7 +286,7 @@ class StocksController < ApplicationController
                                           columns([0]).align=:center
                                           columns([1]).align=:left
                                           columns([2]).align=:left  
-                                          columns([2]).width = 330
+                                          columns([2]).width = 280
                                           columns([3]).align=:left  
                                           columns([4]).align=:right
                                           columns([5]).align=:right 
