0f76171d4c90965ad9275e078e470d62b3852f2f
percy woeds <percywoeds@gmail.com>
2020-02-21 14:23:30 -0500

    a

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -8,18 +8,22 @@ class Assistance < ActiveRecord::Base
     def self.import(file)
           CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
           	
-          	str = row['fecha']
+    #       	str = row['fecha']
 
         
-          	 if str.include?"a. m."		
-          		row['fecha'] = Time.zone.parse(str.sub('a. m.','a.m.'))
-          	else
-				row['fecha'] = Time.zone.parse(str.sub('p. m.','p.m.'))
-          	end
+    #       	 if str.include?"a. m."		
+    #       		row['fecha'] = Time.zone.parse(str.sub('a. m.','a.m.'))
+    #       	else
+				# row['fecha'] = Time.zone.parse(str.sub('p. m.','p.m.'))
+    #       	end
           	
-          	puts "fecha **"
-          	puts str	
-          	puts row['fecha'] 
+    #       	puts "fecha **"
+    #       	puts str	
+    #       	puts row['fecha'] 
+    puts "fecha ==> "
+    puts row['fecha']
+
+
 
             Assistance.create! row.to_hash 
 
