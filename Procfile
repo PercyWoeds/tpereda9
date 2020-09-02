c1be6d7eb4f29ace45421ba687200d33de05cb76
Percy <percywoeds@gmail.com>
2020-09-01 22:21:39 -0500

    a

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 
 web: bin/rails server -p ${PORT:-5000} -e $RAILS_ENV
 worker: bundle exec sidekiq -e $RACK_ENV -C config/sidekiq.yml
+
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,20 @@
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: ENV.fetch('REDISTOGO_URL'), size: 1, network_timeout: 5 }
+# end
+
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: ENV.fetch('REDISTOGO_URL'), size: 12, network_timeout: 5 }
+# end
+
+# Sidekiq::Extensions.enable_delay!
+
+require 'sidekiq'
+
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDISTOGO_URL'), size: 1, network_timeout: 5 }
+config.redis = { :size => 1 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDISTOGO_URL'), size: 12, network_timeout: 5 }
+config.redis = { :size => 4 }
 end
 
-Sidekiq::Extensions.enable_delay!
