f5ba955952e11f66c5392432742a0c2f1f88f707
Percy <percywoeds@gmail.com>
2020-11-05 13:31:00 -0500

    a

--- db/migrate/20201105165734_add_anio1_to_conductors.rb
+++
@@ -1,5 +0,0 @@
-class AddAnio1ToConductors < ActiveRecord::Migration
-  def change
-    add_column :conductors, :anio1, :string
-  end
-end
---
+++ db/migrate/20201105183010_add_anio2_to_conductors.rb
@@ -0,0 +1,9 @@
+class AddAnio2ToConductors < ActiveRecord::Migration
+  def change
+    add_column :conductors, :anio2, :string
+
+    add_column :conductors, :anio3, :string
+add_column :conductors, :anio4, :string
+
+  end
+end
