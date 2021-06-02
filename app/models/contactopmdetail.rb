de021f800079e83616d12ebee8aea250c95048fe
Percy Woeds <percywoeds@gmail.com>
2021-06-02 15:06:10 -0500

    Banco

--- app/models/contactopmdetail.rb
+++ app/models/contactopmdetail.rb
@@ -3,8 +3,6 @@ class Contactopmdetail < ActiveRecord::Base
 
   attr_accessible :sucursal, :contacto, :email, :telefono ,:cargo,:celular,:observa, :contactopmdetails_attributes
 
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  
 
    belongs_to :contactopm
 end
